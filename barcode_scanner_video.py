4ffbbf6d551c954e1ce6f63cb337d7d10db227a0
Gallium Wang <galliumwang199@gmail.com>
2020-11-30 16:30:06 +0800

    complete base barcode module

--- barcode_scanner_video.py
+++ barcode_scanner_video.py
@@ -8,7 +8,7 @@ import time
 import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
+ap.add_argument("-o", "--output", type=str, default="./barcode_data/barcodes.csv",
 	help="path to output CSV file containing barcodes")
 args = vars(ap.parse_args())
 
@@ -21,7 +21,7 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 # open the output CSV file for writing and initialize the set of
 # barcodes found thus far
-csv = open(args["output"], "a")
+# csv = open(args["output"], "a")
 found = set()
 
 # loop over the frames from the video stream
@@ -31,4 +31,40 @@ while True:
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 	# find the barcodes in the frame and decode each of the barcodes
-	barcodes = pyzbar.decode(frame)
+	barcodes = pyzbar.decode(frame)	# loop over the detected barcodes
+	for barcode in barcodes:
+		# extract the bounding box location of the barcode and draw
+		# the bounding box surrounding the barcode on the image
+		(x, y, w, h) = barcode.rect
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+		# the barcode data is a bytes object so if we want to draw it
+		# on our output image we need to convert it to a string first
+		barcodeData = barcode.data.decode("utf-8")
+		barcodeType = barcode.type
+		# draw the barcode data and barcode type on the image
+		text = "{} ({})".format(barcodeData, barcodeType)
+		cv2.putText(frame, text, (x, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		# if the barcode text is currently not in our CSV file, write
+		# the timestamp + barcode to disk and update the set
+		if barcodeData not in found:
+			# csv.write("{},{}\n".format(datetime.datetime.now(),
+			# 	barcodeData))
+			# csv.flush()
+			found.add(barcodeData)
+
+	# show the output frame
+	cv2.imshow("Barcode Scanner", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+# close the output CSV file do a bit of cleanup
+print("[INFO] cleaning up...")
+
+# csv.close()
+cv2.destroyAllWindows()
+vs.stop()
+
+
