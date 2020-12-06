76bd8d00e76e4edd1a4381f764a87fc5144cc187
Gallium Wang <galliumwang199@gmail.com>
2020-12-06 12:21:38 +0800

    backup for code scan module

--- ConcurrencyTest.py
+++ ConcurrencyTest.py
@@ -6,19 +6,14 @@ import threading
 from barcode_scanner_video import StartBarcodeScan
 
 ap = argparse.ArgumentParser()
-
 ap.add_argument("-o", "--output", type=str, #default="./barcode_data/barcodes.csv",
 	help="path to output CSV file containing barcodes")
-
 ap.add_argument("-c", "--camera", type=str, default="pc",
 	help="choose")
-
 args = vars(ap.parse_args())
 if args["output"]:
 	pass
-
 print("[INFO] 启动摄像头...")
-
 #select camera
 if(args["camera"]=="pc"):
 	vs = VideoStream(src=0).start()	#running on pc
@@ -27,15 +22,13 @@ elif(args["camera"]=="ras"):
 else:
 	print("camera parameter is wrong")
 	exit(1)
-
-
 time.sleep(2.0)
 print("摄像头开启")
-
 #prevent one code to be add to found list multipletimes in short time
 found = set()
 
 BarcodeScanThread = threading.Thread(target=StartBarcodeScan, args=(found,vs,))
+
 BarcodeScanThread.start()
 BarcodeScanThread.join()
 
