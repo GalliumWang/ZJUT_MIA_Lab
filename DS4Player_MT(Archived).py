704d5920874d8fd60c23bb3073e4e3cc7207e294
Gallium Wang <galliumwang199@gmail.com>
2020-12-25 19:46:32 +0800

    pre for DS4 mt music play

--- DS4Player_MT(Archived).py
+++ DS4Player_MT(Archived).py
@@ -1,96 +1,107 @@
 import musicalbeeps
 import pygame
-import threading
-
-# FIXME real time response
-#
 
 
-player = musicalbeeps.Player(volume=0.3, mute_output=False)
+player = musicalbeeps.Player(volume = 0.3,mute_output = False)
 
+def note_play(note:str,scale:int=4,duration:float=0.2,player=player):
+    noteConverter = {"do": "C", "re": "D", "mi": "E", "fa": "F", "so": "G", "la": "A", "si":"B"}
 
-def note_play(note: str, scale: int = 4, duration: float = 0.5, player=player):
-    noteConverter = {"do": "C", "re": "D", "mi": "E", "fa": "F", "so": "G", "la": "A", "si": "B"}
-
-    trueNote = noteConverter[note]
-    scale = str(scale)
-    playLoads = trueNote + scale
+    trueNote=noteConverter[note]
+    scale=str(scale)
+    playLoads=trueNote+scale
 
     player.play_note(playLoads, duration)
 
 
-# init
+
 pygame.init()
 j = pygame.joystick.Joystick(0)
 j.init()
 
-currentNote = None
-playingThread = None
-currentScale = 4
-playSwitch = True
+currentScale=4
 
-
-def bgPlay():
-    while True:
-        if (currentNote == None):
-            continue
-        note_play(currentNote, currentScale)
-
-
-playingThread = threading.Thread(target=bgPlay)
-playingThread.start()
+playSwitch=True
 
 try:
     while True:
-        if (playSwitch):
-            for event in pygame.event.get():
+        if(playSwitch):
+            events = pygame.event.get()
+            for event in events:
                 if event.type == pygame.JOYBUTTONDOWN:
                     if j.get_button(4):
-                        currentNote = "do"
+                        #print("L1")
+                        note_play("do",currentScale)
                     elif j.get_button(5):
-                        currentNote = "re"
+                        #print("R1")
+                        note_play("re", currentScale)
                     elif j.get_button(6):
-                        currentNote = "mi"
+                        #print("L2")
+                        note_play("mi", currentScale)
                     elif j.get_button(7):
-                        currentNote = "fa"
-                    elif j.get_button(3):
-                        currentNote = "so"
-                    elif j.get_button(2):
-                        currentNote = "la"
+                        #print("R2")
+                        note_play("fa", currentScale)
+                    elif j.get_button(0):
+                        #print("X")
+                        pass
                     elif j.get_button(1):
-                        currentNote = "si"
+                        #print("CIRCLE")
+                        note_play("si", currentScale)
+                    elif j.get_button(2):
+                        #print("TRIANGLE")
+                        note_play("la", currentScale)
+                    elif j.get_button(3):
+                        #print("SQUARE")
+                        note_play("so", currentScale)
                     elif j.get_button(8):
-                        if currentScale > 1:
-                            currentScale -= 1
+                        #print("SHARE")
+                        if(currentScale>1):
+                            currentScale-=1
                             print("音阶下降:{}".format(currentScale))
                         else:
                             print("已经到达最低音阶")
                     elif j.get_button(9):
-                        if currentScale < 8:
-                            currentScale += 1
+                        #print("OPTION")
+                        if(currentScale<8):
+                            currentScale+=1
                             print("音阶上升:{}".format(currentScale))
                         else:
                             print("已经到达最高音阶")
                     elif j.get_button(10):
-                        # print("POWER BUTTON")
-                        playSwitch = False
-                        currentNote = None  # 暂停后台播放线程
+                        #print("POWER BUTTON")
+                        playSwitch=False
                         print("停止演奏")
 
-                elif event.type == pygame.JOYBUTTONUP:
-                    # FIXME
-                    # figure out code of corresponding get_button method
-                    currentNote=None
+                    # elif j.get_button(11):
+                    #     print("RIGHT ANALOG PRESS")   #FIXME:what button is applied to num 11
 
+                    # elif j.get_button(12):
+                    #     #print("RIGHT STICK PRESS")
+                    #     if(currentScale<8):
+                    #         currentScale+=1
+                    #         print("音阶上升")
+                    #     else:
+                    #         print("已经到达最高音阶")
+                    # elif j.get_button(13):
+                    #     #print("LEFT STICK PRESS")
+                    #     if(currentScale>1):
+                    #         currentScale-=1
+                    #         print("音阶下降")
+                    #     else:
+                    #         print("已经到达最低音阶")     #FIXME:fix crash for button apllied to num 13
+
+                elif event.type == pygame.JOYBUTTONUP:
+                    # print("TOUCHPAD PRESS")
+                    pass
         else:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.JOYBUTTONDOWN:
                     if j.get_button(10):
-                        playSwitch = True
-                        print("开始演奏")  #
+                        playSwitch=True
+                        print("开始演奏")
 
 
-except KeyboardInterrupt:  # handle for ctrl+C
+except KeyboardInterrupt:   #handle for ctrl+C
     print("用户结束程序")
     j.quit()
